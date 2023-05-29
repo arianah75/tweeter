c34193b44af73efe20d18a10fdf38e00709b8443
Arian <arianahmadi1@gmail.com>
2023-05-28 20:31:58 -0400

    finalize and images added refactoring and desktop viewpoint add

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,66 +1,84 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
+  // Function to escape potentially unsafe characters
+  const escape = function (str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-const data = [
-  {
-    user: {
-      name: 'Newton',
-      avatars: 'https://i.imgur.com/73hZDYK.png',
-      handle: '@SirIsaac',
-    },
-    content: {
-      text: 'If I have seen further it is by standing on the shoulders of giants',
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: 'Descartes',
-      avatars: 'https://i.imgur.com/nlhLi3I.png',
-      handle: '@rd',
-    },
-    content: {
-      text: 'Je pense , donc je suis',
-    },
-    created_at: 1461113959088,
-  },
-];
+  // error message
+  const errorElement = $('#error-message');
 
-const createTweetElement = function (tweet) {
-  const text = tweet.content.text;
-  const date = tweet.created_at;
-  const { name, avatars, handle } = tweet.user;
+  // The createTweetElement function now uses the timeago library to format the date.
+  const createTweetElement = function (tweet) {
+    const { content: { text }, created_at, user: { name, avatars, handle } } = tweet;
+    const tweetElement = `<article class="tweet" data-tweet-id="${tweet.id}">
+      <header class="tweet-header">
+        <div class="user-profile">
+          <img class="user-icon" src=${escape(avatars)}></img>
+          <p class="user-name">${escape(name)}</p>
+        </div>
+        <div>
+          <p class="user-handle">${escape(handle)}</p>
+        </div>
+      </header>
+      <div class="tweet-text">${escape(text)}</div>
+      <footer class="tweet-footer">
+        <span class="tweet-date">${timeago.format(created_at)}</span>
+        <div class="tweet-response">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
+    return tweetElement;
+  };
 
-  const tweeElement = `<article class="tweet">
-  <header class="tweet-header">
-    <div class="user-profile">
-      <img class="user-icon" src=${avatars}></img>
-      <p class="user-name">${name}</p>
-    </div>
-    <div>
-      <p class="user-handle">${handle}</p>
-    </div>
-  </header>
-  <div class="tweet-text">${text}</div>
-  <footer class="tweet-footer">
-    <span class="tweet-date">${date}</span>
-    <div class="tweet-response">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-    </div>
-  </footer>
-</article>`;
-  return tweeElement;
-};
+  // Function to load tweets
+  const loadTweets = function () {
+    $.getJSON('/tweets', function (tweets) {
+      $('#tweets-container').empty();
+      tweets.forEach((tweet) => {
+        const $tweet = createTweetElement(tweet);
+        $('#tweets-container').prepend($tweet);
+      });
+    });
+  };
 
-// const $tweet = createTweetElement(tweetData);
+  // Initial loading of tweets
+  loadTweets();
 
-const renderTweets = (arrayOfTweets) =>
-  arrayOfTweets.map((element) => createTweetElement(element));
+  // Form submit handler
+  $('form').on('submit', function (event) {
+    event.preventDefault();
 
-const $tweets = renderTweets(data);
+    errorElement.slideUp();
 
+    const textArea = $('#tweet-text');
+    const tweetLength = textArea.val().length;
+
+    let errorMessage;
+
+    if (tweetLength === 0) {
+      errorMessage = '<i class="fa-solid fa-triangle-exclamation" style="color: #ff0000;"></i>' +
+          '<span>Your tweet is empty. Please write something.</span>' +
+          '<i class="fa-solid fa-triangle-exclamation" style="color: #ff0000;"></i>';
+    } else if (tweetLength > 140) {
+      errorMessage = '<i class="fa-solid fa-triangle-exclamation" style="color: #ff0000;"></i>' +
+          '<span>Your tweet is too long. Please make sure it is less than 140 characters.</span>' +
+          '<i class="fa-solid fa-triangle-exclamation" style="color: #ff0000;"></i>';
+    }
+
+    if (errorMessage) {
+      errorElement.html(errorMessage).slideDown().delay(2000).slideUp();
+      return;
+    }
+
+    $.post('/tweets', $(this).serialize(), function () {
+      loadTweets(); // refresh tweets on successful post
+    }).fail(function (err) {
+      console.error('Error occurred during data submission: ', err);
+    });
+  });
+});
